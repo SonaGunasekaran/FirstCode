e80c3a78634b9a872062a85b6b4bc959a2c40d8e
[Sona] <[sona16061999@gmail.com]>
2021-06-23 22:47:00 +0530

    updated program

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace FirstCode
         static void Main(string[] args)
         {
             int varNum = 12;
-            int varNum1 = 2;
+            int varNum1 = 4;
             Console.WriteLine(varNum * varNum1);
             Console.WriteLine("Hello World!");
         }
