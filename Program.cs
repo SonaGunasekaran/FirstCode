1dcbff3fd87ad031afbd8ee9902cc1c9efb5b896
[Sona] <[sona16061999@gmail.com]>
2021-06-23 11:30:10 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirstCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int varNum = 12;
+            int varNum1 = 2;
+            Console.WriteLine(varNum * varNum1);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
